9dedd472b2fbf6e35c770db3fb54f33fb5b75019
Shannon Kroll <skroll912@gmail.com>
2022-10-04 08:28:18 -0700

    start the new project

---
+++ src/redux/store/index.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux'
+import { expenseReducer } from '../reducers/expenses'
+
+const reducer = combineReducers({
+    expenses: expenseReducer,
+})
+const initialState = {}
+const store = createStore(reducer, initialState)
+
+export default store;
